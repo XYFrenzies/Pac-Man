c325678523d50e4b48efe7e4ff8f7d70b36b4bf4
Benjamin McDonald <s172834@students.aie.edu.au>
2020-07-02 16:46:33 +1000

    I am working on the logic of the player and how it collides with walls. I am happy with the progress and will work on it later. I will be wroking on the ai of the pacman.

--- Pacman/PacManv1/StartupApp.cpp
+++ Pacman/PacManv1/StartupApp.cpp
@@ -1,6 +1,6 @@
 #include "StartupApp.h"
 #include "raylib.h"
-StartupApp::StartupApp()
+StartupApp::StartupApp() : m_pacDir(), m_pacPos(), m_screenWidth(), m_screenHeight(), m_tileWidth(), m_tileHeight(), m_pacSpeed()
 {
 }
 
@@ -10,8 +10,35 @@ StartupApp::~StartupApp()
 
 void StartupApp::StartUp()
 {
+	m_screenWidth = MAP_COLS * 30;
+	m_screenHeight = MAP_ROWS * 30;
+
+
 	InitWindow(m_screenWidth, m_screenHeight, "Hello World");
 	SetTargetFPS(60);
+
+	m_pacPos.x = 0;
+	m_pacPos.y = 0;
+	m_pacDir.x = 0;
+	m_pacDir.y = 0;
+
+
+	m_tileWidth = m_screenWidth / MAP_COLS;
+	m_tileHeight = m_screenHeight / MAP_ROWS;
+
+	m_pacSpeed = 2;
+
+	for (int y = 0; y < MAP_ROWS; y++)
+	{
+		for (int x = 0; x < MAP_COLS; x++)
+		{
+			if (m_map[y][x] == START)
+			{
+				m_pacPos.x = x * m_tileWidth;
+				m_pacPos.y = y * m_tileHeight;
+			}
+		}
+	}
 }
 
 void StartupApp::Run()
@@ -24,12 +51,265 @@ void StartupApp::Run()
 
 }
 
+
+
 void StartupApp::Update()
 {
+	// what tile index is pacman on?
+	int currentIndexX = (m_pacPos.x + m_tileWidth * 0.5f) / m_tileWidth;
+	int currentIndexY = (m_pacPos.y + m_tileHeight * 0.5f) / m_tileHeight;
+
+
+	// what tile index is packman on next tile?
+	int nextIndexX = currentIndexX + m_pacDir.x;
+	int nextIndexY = currentIndexY + m_pacDir.y;
+
+	//If the upkey is pressed but hasnt been pressed before
+	if (IsKeyPressed(m_upKey) && ifPressedUp == false)
+	{
+		//If either other direction is being pressed atm.
+		if ((ifPressedDown == true || ifPressedLeft == true || ifPressedRight == true) && m_map[nextIndexY][nextIndexX] == WALL)
+		{
+			while (m_pacPos.y != currentIndexY * m_tileHeight)
+			{
+				m_pacDir.y = 0;
+				m_pacDir.x = 0;
+				if(m_pacPos.y > currentIndexY * m_tileHeight)
+					m_pacPos.y -= 1;
+				if (m_pacPos.y < currentIndexY* m_tileHeight)
+					m_pacPos.y += 1;
+				Draw();
+			}
+			m_pacDir.y = 0;
+			m_pacDir.x = 0;
+			return;
+		}
+		//If the movement is going to collid with a wall.
+		if (m_map[nextIndexY][nextIndexX] == WALL)
+		{
+			while (m_pacPos.y != currentIndexY * m_tileHeight)
+			{
+				m_pacDir.y = 0;
+				m_pacDir.x = 0;
+				if (m_pacPos.y > currentIndexY* m_tileHeight)
+					m_pacPos.y -= 1;
+				if (m_pacPos.y < currentIndexY * m_tileHeight)
+					m_pacPos.y += 1;
+				Draw();
+			}
+			m_pacDir.y = 0;
+			m_pacDir.x = 0;
+			return;
+		}
+		//Increases the direction
+		m_pacDir = {0, -1};
+		ifPressedUp = true;//The player is moving up
+		ifPressedDown = false;
+		ifPressedLeft = false;
+		ifPressedRight = false;
+	}
+	//If the downkey is pressed but has not been pressed before.
+	else if (IsKeyPressed(m_downKey) && ifPressedDown == false)
+	{
+		//If any of the other keys have not been pressed.
+		if ((ifPressedUp == true || ifPressedLeft == true || ifPressedRight == true) && m_map[nextIndexY][nextIndexX] == WALL)
+		{
+			while (m_pacPos.y != currentIndexY * m_tileHeight)
+			{
+				m_pacDir.y = 0;
+				m_pacDir.x = 0;
+				if (m_pacPos.y > currentIndexY* m_tileHeight)
+					m_pacPos.y -= 1;
+				if (m_pacPos.y < currentIndexY * m_tileHeight)
+					m_pacPos.y += 1;
+				Draw();
+			}
+			m_pacDir.y = 0;
+			m_pacDir.x = 0;
+			return;
+		}
+		//If the direction is going to collid with a wall.
+		if (m_map[nextIndexY][nextIndexX ] == WALL)
+		{
+			while (m_pacPos.y != currentIndexY * m_tileHeight)
+			{
+				m_pacDir.y = 0;
+				m_pacDir.x = 0;
+				if (m_pacPos.y > currentIndexY* m_tileHeight)
+					m_pacPos.y -= 1;
+				if (m_pacPos.y < currentIndexY * m_tileHeight)
+					m_pacPos.y += 1;
+				Draw();
+			}
+			m_pacDir.y = 0;
+			m_pacDir.x = 0;
+			return;
+		}
+		//Moves in the direction
+		m_pacDir = { 0, 1 };
+		ifPressedUp = false;
+		ifPressedDown = true;//The player is moving down
+		ifPressedLeft = false;
+		ifPressedRight = false;
+	}
+	//If the leftkey is pressed but has not been pressed before.
+	else if (IsKeyPressed(m_leftKey) && ifPressedLeft == false)
+	{
+		//If any of the other keys have not been pressed.
+		if ((ifPressedDown == true || ifPressedRight == true || ifPressedUp == true) && m_map[nextIndexY][nextIndexX] == WALL)
+		{
+			while (m_pacPos.x != currentIndexX * m_tileWidth)
+			{
+				m_pacDir.y = 0;
+				m_pacDir.x = 0;
+				if (m_pacPos.x > currentIndexX* m_tileWidth)
+					m_pacPos.x -= 1;
+				if (m_pacPos.x < currentIndexX * m_tileWidth)
+					m_pacPos.x += 1;
+				Draw();
+			}
+			m_pacDir.y = 0;
+			m_pacDir.x = 0;
+			return;
+		}
+		//If the direction is going to collid with a wall.
+		if (m_map[nextIndexY][nextIndexX] == WALL)
+		{
+			while (m_pacPos.x != currentIndexX * m_tileWidth)
+			{
+				m_pacDir.y = 0;
+				m_pacDir.x = 0;
+				if (m_pacPos.x > currentIndexX* m_tileWidth)
+					m_pacPos.x -= 1;
+				if (m_pacPos.x < currentIndexX * m_tileWidth)
+					m_pacPos.x += 1;
+				Draw();
+			}
+			m_pacDir.y = 0;
+			m_pacDir.x = 0;
+			return;
+		}
+		//Moves in the direction
+		m_pacDir = { -1, 0 };
+		ifPressedUp = false;
+		ifPressedDown = false;
+		ifPressedLeft = true;//The player is moving left
+		ifPressedRight = false;
+	}	
+	//If the rightkey is pressed but has not been pressed before.
+	else if (IsKeyPressed(m_rightKey) && ifPressedRight == false)
+	{
+		//If any of the other keys have not been pressed.
+		if ((ifPressedDown == true || ifPressedLeft == true || ifPressedUp == true) && m_map[nextIndexY][nextIndexX] == WALL)
+		{
+			while (m_pacPos.x != currentIndexX * m_tileWidth)
+			{
+				m_pacDir.y = 0;
+				m_pacDir.x = 0;
+				if (m_pacPos.x > currentIndexX* m_tileWidth)
+					m_pacPos.x -= 1;
+				if (m_pacPos.x < currentIndexX * m_tileWidth)
+					m_pacPos.x += 1;
+				Draw();
+			}
+			m_pacDir.y = 0;
+			m_pacDir.x = 0;
+			return;
+		}
+		//If the direction is going to collid with a wall.
+		if (m_map[nextIndexY][nextIndexX] == WALL)
+		{
+			while (m_pacPos.x != currentIndexX * m_tileWidth)
+			{
+				m_pacDir.y = 0;
+				m_pacDir.x = 0;
+				if (m_pacPos.x > currentIndexX* m_tileWidth)
+					m_pacPos.x -= 1;
+				if (m_pacPos.x < currentIndexX * m_tileWidth)
+					m_pacPos.x += 1;
+				Draw();
+			}
+			m_pacDir.y = 0;
+			m_pacDir.x = 0;
+			return;
+		}
+		//Moves in the direction
+		m_pacDir = { 1, 0 };
+		ifPressedUp = false;
+		ifPressedDown = false;
+		ifPressedLeft = false;
+		ifPressedRight = true;//The player is moving right
+	}
+
+	m_pacPos.x += m_pacDir.x * m_pacSpeed;
+	m_pacPos.y += m_pacDir.y * m_pacSpeed;
+
+
+	//Checks if the food exists
+	if (m_map[currentIndexY][currentIndexX] == FOOD)
+	{
+		m_map[currentIndexY][currentIndexX] = EMPTY;
+	}
+	//Checks if its near a way.
+	if (m_map[nextIndexY][nextIndexX] == WALL)
+	{
+		while (m_pacPos.x != currentIndexX * m_tileWidth)
+		{
+			m_pacDir.y = 0;
+			m_pacDir.x = 0;
+			if (m_pacPos.x > currentIndexX* m_tileWidth)
+				m_pacPos.x -= 1;
+			if (m_pacPos.x < currentIndexX * m_tileWidth)
+				m_pacPos.x += 1;
+			Draw();
+		}
+		m_pacDir.x = 0;
+		m_pacDir.y = 0;
+		m_pacPos.x = currentIndexX * m_tileWidth;
+		m_pacPos.y = currentIndexY * m_tileHeight;
+	}
+
 }
 
 void StartupApp::Draw()
 {
+	BeginDrawing();
+
+	ClearBackground(BLACK);
+
+	int tw = m_tileWidth;//TileWidth
+	int th = m_tileHeight;//TileHeight
+	int hw = tw / 2; //HalfWidth
+	int hh = th / 2; //HalfHeight
+
+	for (int y = 0; y < MAP_ROWS; y++)
+	{
+		for (int x = 0; x < MAP_COLS; x++)
+		{
+			int xPos = x * tw;
+			int yPos = y * th;
+			int tileID = m_map[y][x];
+
+			if (tileID == WALL)
+			{
+				DrawRectangle(xPos, yPos, tw, th, WHITE );
+			}
+			if (tileID == FOOD)
+			{
+				DrawRectangle(xPos + hw, yPos + hh, tw / 5, th / 5, YELLOW);
+			}
+		}
+	}
+
+	// deraw packman
+	DrawCircle(
+		m_pacPos.x + tw * 0.5f,
+		m_pacPos.y + th * 0.5f,
+		tw * 0.5f - 3,
+		Color({ 255, 255, 0, 255 })
+	);
+
+	EndDrawing();
 }
 
 void StartupApp::ShutDown()
--- Pacman/PacManv1/StartupApp.h
+++ Pacman/PacManv1/StartupApp.h
@@ -1,6 +1,25 @@
 #pragma once
 
 #include "raymath.h"
+
+enum MAP_ITEMS
+{
+	EMPTY = 0,
+	WALL = 1,
+	FOOD = 2,
+	POWERUP = 3,
+	START = 4,
+	GHOST = 5
+};
+
+enum Directions
+{
+	NORTH,
+	SOUTH,
+	EAST,
+	WEST
+};
+
 class StartupApp
 {
 
@@ -24,7 +43,107 @@ private:
 	//Updates the window
 	void Update();
 
-	int m_screenWidth = 1080;
-	int m_screenHeight = 720;
+	int m_screenWidth;//Widthscreen
+	int m_screenHeight;//Heightscreen
+
+	int m_tileWidth;//The width that the individual sizing of the tile will be
+	int m_tileHeight;//The height that the individual sizing of the tile will be
+
+	Vector2 m_pacPos;//The pacmans position
+	Vector2 m_pacDir;//The pacmans position
+
+
+	float m_pacSpeed;//The pacmans speed
+
+	static const int MAP_ROWS = 20;//The amount of rows across
+
+	static const int MAP_COLS = 15;//The amount of coloums long
+
+	KeyboardKey m_leftKey = KEY_LEFT;
+	KeyboardKey m_rightKey = KEY_RIGHT;
+	KeyboardKey m_upKey = KEY_UP;
+	KeyboardKey m_downKey = KEY_DOWN;
+
+	//Security measures to make sure that the player cant go through the diagonal walls
+	bool ifPressedUp = false;
+	bool ifPressedDown = false;
+	bool ifPressedLeft = false;
+	bool ifPressedRight = false;
+
+
+
+	int m_map[MAP_ROWS][MAP_COLS] = {
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+	{1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1},
+	{1, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 2, 1, 2, 1},
+	{1, 2, 2, 2, 1, 1, 2, 1, 2, 1, 1, 2, 2, 2, 1},
+	{1, 2, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 2, 1},
+	{1, 2, 1, 2, 2, 1, 2, 1, 1, 1, 2, 2, 2, 2, 1},
+	{1, 2, 1, 1, 2, 2, 2, 5, 5, 1, 2, 1, 1, 2, 1},
+	{1, 2, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1, 2, 1},
+	{1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 1, 2, 1},
+	{1, 2, 1, 1, 2, 1, 2, 1, 1, 2, 1, 2, 2, 2, 1},
+	{1, 2, 1, 1, 2, 1, 2, 2, 2, 2, 1, 2, 1, 2, 1},
+	{1, 2, 1, 2, 2, 2, 1, 2, 1, 2, 2, 2, 1, 2, 1},
+	{1, 2, 1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1},
+	{1, 2, 1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1},
+	{1, 2, 2, 2, 1, 2, 2, 4, 1, 1, 2, 2, 2, 2, 1},
+	{1, 2, 1, 2, 2, 1, 1, 2, 1, 2, 2, 1, 1, 2, 1},
+	{1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1},
+	{1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+
+	// Programming ideas
+/*
+
+	- bullet hell
+	- tank game
+	- liquid simulation
+	- tetris
+	- pong (in scope)
+	- pacman (in scope)
+	- - map
+	- - - empty     0
+	- - - walls		1
+	- - - food		2
+	- - - powerup	3
+
+	[
+
+		[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+		[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+		[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+		[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+		[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+		[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+		[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+		[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+		[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+		[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+		[1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+		[1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+		[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+		[1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+		[1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+		[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+		[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+
+	]
+
+
+	- - player
+	- -
+	- space invaders (maybe in scope)
+	- render a 3d cube (); maybe in scope wiht build in raylib
+	- fps (not in scope)
+	- wackamol (maybe in scope)
+	- snake (in scope)
+	- frogger (maybe in scope)
+	- duck hunt (in scope)
+	- bad particle system (maybe in scope)
+	- breakout (in scope)
+
+*/
 };
 
